0c66b0c6f0567a8388b10af5b0db13eec829ebd4
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-05 17:37:05 +0500

    feat: Add starlight integration and custom CSS for SaltLakeCity.dental

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    starlight({
+      title: "SaltLakeCity.dental",
+      customCss: [
+        "./src/styles/docs.css",
+        "@fontsource-variable/mada",
+        "@fontsource-variable/montserrat",
+        "@fontsource/palanquin/300.css",
+        "@fontsource/palanquin/400.css",
+        "@fontsource/palanquin/500.css",
+        "@fontsource/palanquin/600.css",
+      ],
+      
+    }),
+  ],
 });
