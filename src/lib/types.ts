fa304d617ca5fbfd955824f75209c05c2c7aadf3
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-05 12:50:04 +0500

    feat: Add types for LandingPage component

---
+++ src/lib/types.ts
@@ -0,0 +1,52 @@
+export type LandingPage = {
+  title: string;
+  navItems: {
+    title: string;
+    url: string;
+  }[];
+  navButton: {
+    title: string;
+    url: string;
+  };
+  hero: {
+    title: string;
+    subtitle: string;
+    button: {
+      title: string;
+      url: string;
+    };
+  };
+  meetTheDoctor: {
+    title: string;
+    doctorName: string;
+    doctorDescription: string;
+    doctorProfile: ImageMetadata;
+    expertise: {
+      title: string;
+      list: string[];
+    };
+  };
+  transformingSmiles: {
+    title: string;
+    before: {
+      description: string;
+      image: ImageMetadata;
+    };
+    after: {
+      description: string;
+      image: ImageMetadata;
+    };
+  };
+  services: {
+    title: string;
+    description: string;
+    button: {
+      title: string;
+      url: string;
+    };
+    list: {
+      description: string;
+      image: ImageMetadata;
+    }[];
+  };
+};
