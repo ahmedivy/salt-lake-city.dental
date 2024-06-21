1a7d1a3f2ca50716bd838fb12e2281d1eb8baab5
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-06-22 00:24:11 +0500

    chore: Update Astro configuration with React integration and custom CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,24 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import starlightBlog from "starlight-blog";
-
 import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    starlight({
-      title: "SaltLakeCity.dental",
-      customCss: [
-        "./src/styles/docs.css",
-        "@fontsource-variable/mada",
-        "@fontsource-variable/montserrat",
-        "@fontsource/palanquin/300.css",
-        "@fontsource/palanquin/400.css",
-        "@fontsource/palanquin/500.css",
-        "@fontsource/palanquin/600.css",
-      ],
-      plugins: [starlightBlog({})],
-      sidebar: [
-        {
-          label: "Docs",
-          autogenerate: { directory: "docs" },
-        },
-      ],
-      components: {
-        ThemeSelect: "./src/components/ThemeSelect.astro",
+  integrations: [tailwind(), starlight({
+    title: "SaltLakeCity.dental",
+    customCss: ["./src/styles/docs.css", "@fontsource-variable/mada", "@fontsource-variable/montserrat", "@fontsource/palanquin/300.css", "@fontsource/palanquin/400.css", "@fontsource/palanquin/500.css", "@fontsource/palanquin/600.css"],
+    plugins: [starlightBlog({})],
+    sidebar: [{
+      label: "Docs",
+      autogenerate: {
+        directory: "docs"
       }
-    }),
-  ],
+    }],
+    components: {
+      ThemeSelect: "./src/components/ThemeSelect.astro"
+    }
+  }), react()]
 });
